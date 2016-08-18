7679a885a1fc88d1761def4b2d081ce7bc3b5c84
cezmunsta <ceri.williams@percona.com>
2016-08-18 17:41:39 +0100

    Updated smysql to work with other programs

--- bin/smysql.sh
+++ bin/smysql.sh
@@ -1,27 +1,69 @@
 #!/bin/bash
 
-declare -ar ARGS=( ${@} )
+set -eu
+
+declare -ra ARGS=( ${@} )
 declare -ri ARGV=${#ARGS}
-declare -r SEC_MYCNF=${1:-my.gpg}
+declare -rA ALIASES=(
+  [smysql]=mysql
+  [sgrants]=pt-show-grants
+  [schecksum]=pt-table-checksum
+  [ssync]=pt-table-sync
+  [sdigest]=pt-query-digest
+)
+declare -r PROGNAME=$(basename ${0})
+declare -r SEC_MYCNF=$(test -f ${1:-undef} && echo $_ || echo 'my.gpg')
 declare -r SEC_FIFO=$(mktemp)
-declare -r PASSTHRU=${ARGS[@]:1}
+declare -r PASSTHRU=${ARGS[@]:$(test -f ${1:-undef} && echo 1 || echo 0)}
 
 function cleanup {
-    test -f ${SEC_FIFO} && rm -f $_
+    test -e ${SEC_FIFO} && rm -f $_
     return $?
 }
 
 function decrypt {
+    set +e
     $(which gpg) --batch --yes -o ${SEC_FIFO} -d ${SEC_MYCNF} >debug.log 2>&1
-    test $? -eq 0 || $(which gpg) -o ${SEC_FIFO} -d ${SEC_MYCNF} >debug.log 2>&1 
+    test $? -eq 0 || $(which gpg) --yes -o ${SEC_FIFO} -d ${SEC_MYCNF} >debug.log 2>&1
+    set -e
 }
 
-function mysql_connect {
-    $(which mysql) --defaults-file=${SEC_FIFO} ${@}
+function check_cmd {
+    local k
+    local cmd=${1}
 
+    for k in "${!ALIASES[@]}"; do
+        test "${cmd}" = ${k} && \
+          test -x "$(which ${ALIASES[${k}]})" && \
+            echo $_ && return 0
+    done
+
+    return 1
+}
+
+function exec_cmd {
+    local -r cmd=${1}
+    local -a args=( ${@} )
+    local -r passthru=${args[@]:1}
+
+    ${cmd} --defaults-file=${SEC_FIFO} ${passthru[@]}
+}
+
+function usage {
+    cat <<EOS | fold -sw 70
+USAGE: $(basename ${0}) enc_file.gpg [--arg=val]
+
+currently supports:
+${ALIASES[@]}
+
+EOS
 }
 
 trap cleanup EXIT
 
+test -e ${SEC_MYCNF} || { usage; exit 1; }
+cmd=$(check_cmd ${PROGNAME})
+test $? -eq 0 || { echo ${ALIASES[${PROGNAME}]} is not available; exit 3; }
+
 cleanup && mkfifo ${SEC_FIFO} && decrypt &
-mysql_connect ${PASSTHRU}
+exec_cmd ${cmd} ${PASSTHRU[@]}
